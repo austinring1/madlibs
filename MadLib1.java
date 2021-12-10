91fb31e6761757eab50265ca009fabc6c3d198a4
Austin Ring <34179431+austinring1@users.noreply.github.com>
2021-12-10 13:16:52 -0800

    Add files via upload

---
+++ MadLib1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main
+{
+    Public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a noun ")
+        
+
+
+        String lib = "The sussy <noun> snuck up behind the <adjective> crewmate and <verb>.";
+        String nounIndex = lib.indexOf("noun")
+        lib.replace("noun", noun)
+        String adjectiveIndex = lib.indexOf("adjective")
+        String lib.replace("adjective", noun)
+        nounIndex = lib.indexOf("noun")
+        lib.replace("noun", noun)
+    }
+}
